23d8965f89023bb6c58ffcbfe77e2b2a54a9b8ee
Evan Vargas <evanvargas@gmail.com>
2023-01-29 16:02:05 -0600

    Made a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<ExpenseItem />
+		</div>
+	);
 }
 
 export default App;
