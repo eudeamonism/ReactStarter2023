30aa33854abbc819e1df5cf50bad710466abf4f8
Evan Vargas <evanvargas@gmail.com>
2023-01-30 16:16:56 -0600

    Card Wrapper

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
 	const month = props.date.toLocaleString('en-US', { month: 'long' });
 	const year = props.date.toLocaleString('en-US', { year: 'numeric' });
 	const day = props.date.toLocaleString('en-US', { day: 'numeric' });
 	return (
-		<div>
-			<div>{month}</div>
-			<div>{year}</div>
-			<div>{day}</div>
+		<div className="expense-date">
+			<div className="expense-date__month">{month}</div>
+			<div className="expense-date__year">{year}</div>
+			<div className="expense-date__day">{day}</div>
 		</div>
 	);
 }
