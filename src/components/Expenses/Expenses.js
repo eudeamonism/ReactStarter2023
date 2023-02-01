243af84870ee17fa6e2b4c549dfc606c2c438c97
Evan Vargas <evanvargas@gmail.com>
2023-01-31 21:59:15 -0600

    Conditionals with if statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,23 +14,27 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
+	let expensesContent = <p>Empty</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<div>
 			<Card className="expenses">
 				<ExpenseFilter
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
-				/>
-				{filteredExpenses.length === 0 && <p>Empty</p>}
-				{filteredExpenses.length > 0 &&
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))}
+                />
+                {expensesContent}
 			</Card>
 		</div>
 	);
