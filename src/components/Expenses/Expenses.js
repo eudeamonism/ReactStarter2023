6b60f4acc9209fc51a374aac4511a80573a552b3
Evan Vargas <evanvargas@gmail.com>
2023-01-31 21:53:08 -0600

    Ternary, empty message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,13 +7,12 @@ import './Expenses.css';
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState('2020');
 	const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear);
+		setFilteredYear(selectedYear);
 	};
 
 	const filteredExpenses = props.items.filter((expense) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
-    console.log(filteredExpenses)
 
 	return (
 		<div>
@@ -22,14 +21,16 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{filteredExpenses.map((expense) => (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				))}
+				{filteredExpenses.length === 0 && <p>Empty</p>}
+				{filteredExpenses.length > 0 &&
+					filteredExpenses.map((expense) => (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					))}
 			</Card>
 		</div>
 	);
